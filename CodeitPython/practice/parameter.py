6c0d18ffdaa4f4ffedabe555a68e11447b0e54e5
Turtlefromocean <oliviajiyoungshin@gmail.com>
2021-02-22 22:32:48 +0900

    02.22/프로그래밍기초 in python/토픽1

---
+++ CodeitPython/practice/parameter.py
@@ -0,0 +1,14 @@
+# 08. 파라미터
+def hello(name):
+    print("Hello!")
+    print(name)
+    print("Welcome to Codeit!")
+
+hello("Michael")
+
+# 09. 여러개의 파라미터
+
+def print_sum(a, b, c):
+    print((a + b) * c)
+
+print_sum(7, 3, 10)
